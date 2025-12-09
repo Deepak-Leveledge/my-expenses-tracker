dbd12e0aa52fd5091e9ca9a9af0877f710383e1a
Deepak-Leveledge <deepakleveledge@gmail.com>
2025-12-09 12:04:59 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from mcp.server.fastmcp import FastMCP
+
+# Create MCP instance
+mcp = FastMCP("Expenses-tracker-mcp-server")
+
+# Import your tools AFTER mcp is created
+from main import *   # all tools, prompts, resources attach to mcp
+
+# Create ASGI application
+app = mcp.http_app()

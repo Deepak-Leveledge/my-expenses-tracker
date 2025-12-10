1200478ff35c656d05e1007f24c8cbe545e1539c
Deepak-Leveledge <deepakleveledge@gmail.com>
2025-12-10 13:21:48 +0530

    commented app.py

--- main.py
+++ main.py
@@ -635,7 +635,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     mcp.run(
         transport="streamable-http",
-        host="0.0.0.0",
+        # host="0.0.0.0",
         port=port
     )
 
